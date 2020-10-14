88bd47eac4cee7a5128aca39ffbb1b04ab6b246c
Zlatko Rusev <zlatko.rusev@gmail.com>
2020-10-14 22:20:44 +0300

    Create request fluent validation added

--- TimeOffManager/TimeOffManager.Application/Vacations/Requests/Commands/Create/CreateRequestCommandValidator.cs
+++ TimeOffManager/TimeOffManager.Application/Vacations/Requests/Commands/Create/CreateRequestCommandValidator.cs
@@ -1,6 +1,29 @@
 ﻿namespace TimeOffManager.Application.Vacations.Requests.Commands.Create
 {
-    public class CreateRequestCommandValidator
+    using FluentValidation;
+    using static Domain.Vacations.Models.ModelConstants.Request;
+
+    public class CreateRequestCommandValidator : AbstractValidator<CreateRequestCommand>
     {
+        public CreateRequestCommandValidator()
+        {
+            this.RuleFor(u => u.RequesterComment)
+                .MaximumLength(MaxCommentLength);
+
+            this.RuleFor(u => u.Start)
+                .NotEmpty();
+
+            this.RuleFor(u => u.End)
+                .NotEmpty();
+
+            this.RuleFor(d => d.End)
+                .GreaterThanOrEqualTo(e => e.Start);
+
+            this.RuleFor(u => u.Hours)
+                .NotEmpty();
+
+            this.RuleFor(u => u.RequestTypeName)
+                .NotEmpty();
+        }
     }
 }
